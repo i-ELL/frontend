5bc76120af01688b08bbcde55401cdba8791632f
Elena Starukhina <87142229+i-ELL@users.noreply.github.com>
2024-05-23 23:28:39 +0300

    Update lk.js удаление инфы о себе

--- src/pages/lk.js
+++ src/pages/lk.js
@@ -75,40 +75,7 @@ export default function EditButton() {
                             <Link to="/test">
                                 <MDBBtn className='w-100 mb-4 gradient-custom-4' size='md'>Перейти к тестам</MDBBtn>
                             </Link>
-                            <MDBCardBody className="text-black p-4">
-                                <div className="mb-5">
-                                    <p className="lead fw-normal mb-1">Напишите информацию о себе</p>
-                                    <div className="p-4" style={{backgroundColor: '#f8f9fa'}}>
-                                        <MDBCardText className="font-italic mb-1">Web Developer</MDBCardText>
-
-                                    </div>
-                                </div>
-                                <div className="d-flex justify-content-between align-items-center mb-4">
-                                    <MDBCardText className="lead fw-normal mb-0">Статистика</MDBCardText>
-
-                                </div>
-
-                                {/*<MDBRow>*/}
-                                {/*    <MDBCol className="mb-2">*/}
-                                {/*        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"*/}
-                                {/*                      alt="image 1" className="w-100 rounded-3" />*/}
-                                {/*    </MDBCol>*/}
-                                {/*    <MDBCol className="mb-2">*/}
-                                {/*        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"*/}
-                                {/*                      alt="image 1" className="w-100 rounded-3" />*/}
-                                {/*    </MDBCol>*/}
-                                {/*</MDBRow>*/}
-                                {/*<MDBRow className="g-2">*/}
-                                {/*    <MDBCol className="mb-2">*/}
-                                {/*        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"*/}
-                                {/*                      alt="image 1" className="w-100 rounded-3" />*/}
-                                {/*    </MDBCol>*/}
-                                {/*    <MDBCol className="mb-2">*/}
-                                {/*        <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"*/}
-                                {/*                      alt="image 1" className="w-100 rounded-3" />*/}
-                                {/*    </MDBCol>*/}
-                                {/*</MDBRow>*/}
-                            </MDBCardBody>
+                            
                         </MDBCard>
                     </MDBCol>
                 </MDBRow>
